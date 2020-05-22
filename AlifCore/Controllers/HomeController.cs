7a1c26760c71109913265d8ed93a16b883ca6544
C#Developer <shahzodkiller11@gmail.com>
2020-05-22 14:07:44 +0500

    Finished

---
+++ AlifCore/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using AlifCore.Models;
+using Dapper;
+using System.Data.SqlClient;
+
+namespace AlifCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            using (var context = new SqlConnection(GetStringCon()))
+            {
+                var list = context.Query<Person>("SELECT * FROM Person");
+                return View(list);
+            }
+        }
+        static string GetStringCon()
+        {
+            return "Data Source=localhost;Initial Catalog=Test;Integrated Security=True";
+        }
+    }
+}
